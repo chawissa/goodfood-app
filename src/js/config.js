eeef9d062c71cffaf1ee3b6f7461677b96cdd62c
Charissa Ramirez <chawissa@gmail.com>
2021-09-03 11:11:07 -0400

    Post method - add recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RESULTS_PER_PAGE = 10;
-export const KEY = "fa37ac10-81a0-4b95-813e-2e384fcb280b";
+export const KEY = "8a38e18d-1c35-4fb8-8ed1-6f5e0984a802";
 export const MODAL_CLOSE_SEC = 2.5;
